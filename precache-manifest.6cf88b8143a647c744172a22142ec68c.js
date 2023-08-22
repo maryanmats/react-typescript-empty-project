03a1b26aece04c95beb60d60e96fe172031c7fcf
Maryan Mats <cholgyni@gmail.com>
2023-08-22 22:31:28 +0300

    Updates

---
+++ precache-manifest.6cf88b8143a647c744172a22142ec68c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b27982767aec579a147cd50ffd80456",
+    "url": "/react-typescript-empty-project/index.html"
+  },
+  {
+    "revision": "4dede2c1348e307809ab",
+    "url": "/react-typescript-empty-project/static/css/main.76cf170d.chunk.css"
+  },
+  {
+    "revision": "161beefb8c9b512f0115",
+    "url": "/react-typescript-empty-project/static/js/2.7328ffcf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-typescript-empty-project/static/js/2.7328ffcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dede2c1348e307809ab",
+    "url": "/react-typescript-empty-project/static/js/main.21341b11.chunk.js"
+  },
+  {
+    "revision": "4b2d76651a7f2665254a",
+    "url": "/react-typescript-empty-project/static/js/runtime-main.f7dc8200.js"
+  }
+]);
